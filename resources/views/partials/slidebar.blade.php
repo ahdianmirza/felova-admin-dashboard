2566b6fb41f8a59ab83f6b786fcf6c438b675ece
ahdianmirza <ahdianmirza@gmail.com>
2023-08-30 15:05:50 +0700

    memperbaiki button logout slider

--- resources/views/partials/slidebar.blade.php
+++ resources/views/partials/slidebar.blade.php
@@ -74,11 +74,11 @@
                 </a>
             </li>
             <li class="relative py-2">
-                <form action="/logout" method="post"
-                    class="group flex items-center cursor-pointer truncate py-4 pl-7 rounded-[5px] text-[0.875rem] text-[#B4B990] outline-none transition duration-300 ease-linear hover:bg-[#b2b79f] hover:text-white hover:outline-none focus:outline-none active:bg-primary-400/10 active:text-primary-600 active:outline-none data-[te-sidenav-state-active]:text-primary-600 data-[te-sidenav-state-focus]:outline-none motion-reduce:transition-none dark:text-gray-300 dark:hover:bg-white/10 dark:focus:bg-white/10 dark:active:bg-white/10"
-                    href="/logout" data-te-sidenav-link-ref>
+                <form action="/logout" method="post" href="/logout">
                     @csrf
-                    <button type="submit">
+                    <button type="submit"
+                        class="group w-full flex items-center cursor-pointer truncate py-4 pl-7 rounded-[5px] text-[0.875rem] text-[#B4B990] outline-none transition duration-300 ease-linear hover:bg-[#b2b79f] hover:text-white hover:outline-none focus:outline-none active:bg-primary-400/10 active:text-primary-600 active:outline-none data-[te-sidenav-state-active]:text-primary-600 data-[te-sidenav-state-focus]:outline-none motion-reduce:transition-none dark:text-gray-300 dark:hover:bg-white/10 dark:focus:bg-white/10 dark:active:bg-white/10"
+                        data-te-sidenav-link-ref>
                         <div class="flex justify-center items-center">
                             <span
                                 class="mr-4 [&>svg]:h-[26px] [&>svg]:w-[26px] [&>svg]:transition [&>svg]:duration-300 [&>svg]:ease-linear group-[te-sidenav-state-active]:[&>svg]:fill-primary-600 motion-reduce:[&>svg]:transition-none dark:[&>svg]:fill-gray-300 dark:group-hover:[&>svg]:fill-gray-300 ">
